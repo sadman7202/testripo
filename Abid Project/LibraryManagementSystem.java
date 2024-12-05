361dbfb734febb555fe1d4df19fc43b9df2cf318
sadman7202 <148948408+sadman7202@users.noreply.github.com>
2024-12-05 21:27:27 +0600

    odhik shihoron

--- Abid Project/LibraryManagementSystem.java
+++ Abid Project/LibraryManagementSystem.java
@@ -4,7 +4,15 @@ package lms;
 import java.util.*;
 
 class LibraryManagementSystem {
+    Scanner scanner = new Scanner(System.in);
     private List<Book> books = new ArrayList<>();
+    {
+        books.add(new Book(1, "Harry Potter and the Sorcerer's Stone","J.K. Rowling"));
+        books.add(new Book(2, "Dune","Frank Herbert"));
+        books.add(new Book(3, "Gone Girl","Gillian Flynn"));
+        books.add(new Book(4, "Pather Panchali","Bibhutibhushan Bandopadhyay"));
+        books.add(new Book(5, "Shesher Kobita","Rabindranath Tagore"));
+    }
 
     // Add a book
     public void addBook(Book book) {
@@ -52,9 +60,16 @@ class LibraryManagementSystem {
         return null;
     }
     private static List<Member> members = new ArrayList<>();
+    {
+        members.add(new Member("Mehedi Hasan", "241-50-001"));
+        members.add(new Member("Fahim Shahriar", "241-50-002"));
+        members.add(new Member("Taohid Ayon", "241-50-003"));
+        members.add(new Member("Sadman Sakib", "241-50-004"));
+        members.add(new Member("Abid Chowdhury", "241-50-011"));
+    }
     public void addMembers() {
 
-        Scanner scanner = new Scanner(System.in);
+
 
         System.out.print("Enter Member name: ");
 
@@ -86,5 +101,57 @@ class LibraryManagementSystem {
 
             System.out.println("------------------------");
         }
+    }
+    int count;
+    public admin[] theAdmins=new admin[10];
+    public void addAdmin(admin a)
+    {
+        for (int i = 0; i < count; i++) {
+
+            if (a.regNum.equalsIgnoreCase(
+                    theAdmins[i].regNum)) {
+
+                // Print statement
+                System.out.println(
+                        "Admin of Reg Num " + a.regNum
+                                + " is Already Registered.");
+
+                return;
+            }
+        }
+        if(a.getPin!=a.pin){
+            System.out.println("!! WRONG PINCODE !!");
+            return;
+        }
+
+        if (count <= 10) {
+            theAdmins[count] = a;
+            count++;
+            System.out.println("ADMIN REGISTERD !");
+        }
+    }
+    public int Adminlogin()
+    {
+        // Display message only
+        System.out.print("Enter Reg Number: ");
+
+        String regNum = scanner.nextLine();
+        System.out.print("\nEnter Password: ");
+        String pass=scanner.nextLine();
+
+
+        for (int i = 0; i < count; i++) {
+
+            if (theAdmins[i].regNum.equalsIgnoreCase(regNum) &&
+                    theAdmins[i].password.equals(pass)) {
+                return 1;
+            }
+        }
+
+        // Print statements
+        System.out.println("Admin is not Registered.");
+        System.out.println("Get Registered First.");
+
+        return -1;
     }
 }
